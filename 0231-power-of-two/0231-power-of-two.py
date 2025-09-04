4806155ac735ff1d577b722c3961b38a3e596480
Yusuf Tufan <102178687+yusuf-tufan@users.noreply.github.com>
2025-09-04 17:03:22 +0300

    Time: 315 ms (1.97%), Space: 17.8 MB (55.64%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        new_list=[]
+        for c in range(0,1000):
+            k=2**c
+            new_list.append(k)
+        if n in new_list:
+            return True
+        else:
+            return False
