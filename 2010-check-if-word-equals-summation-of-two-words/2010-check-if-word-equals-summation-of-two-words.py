f3e595ace766fae29a0753cb29652f9b3b2214f4
Yusuf Tufan <102178687+yusuf-tufan@users.noreply.github.com>
2025-09-12 18:27:00 +0300

    Time: 0 ms (100%), Space: 12.5 MB (60.81%) - LeetHub

--- 2010-check-if-word-equals-summation-of-two-words/2010-check-if-word-equals-summation-of-two-words.py
+++ 2010-check-if-word-equals-summation-of-two-words/2010-check-if-word-equals-summation-of-two-words.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def isSumEqual(self, firstWord, secondWord, targetWord):
-        lowercase_letters = [
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-        ]
+        lowercase_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
         first_list = []
         second_list=[]
         target_list=[]
