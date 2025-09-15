b3b478973fa550836bc5dd3d7c58931859a926d3
Yusuf Tufan <102178687+yusuf-tufan@users.noreply.github.com>
2025-09-15 12:48:10 +0300

    Time: 4917 ms (5.19%), Space: 12.4 MB (82.96%) - LeetHub

---
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def canBeTypedWords(self, text, brokenLetters):
+        new_text=text.split()
+        for c in range(len(new_text)):
+            for i in brokenLetters:
+                for x in new_text:
+                    print(x)
+                    if i in x:
+                        new_text.remove(x)
+        return len(new_text)
