9ae5426355772ee83925391fdefca2b207143d7b
Yusuf Tufan <102178687+yusuf-tufan@users.noreply.github.com>
2025-09-16 12:58:05 +0300

    Time: 8 ms (77.43%), Space: 12.6 MB (17.71%) - LeetHub

---
+++ 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def secondHighest(self, s):
+        new_list=[]
+        for i in s:
+            if i.isdigit():
+                new_list.append(i)
+        new_list=list(set(new_list))
+        if len(new_list)==0:
+            return -1
+        else:
+            if len(new_list)==1:
+                return -1
+            else:
+                new_list.sort()
+                new_list.remove(new_list[-1])
+
+                return int(new_list[-1])
