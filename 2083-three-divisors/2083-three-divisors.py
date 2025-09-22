ee58819050c68d7dcf881a91c1b2633f4a4fd0e4
Yusuf Tufan <102178687+yusuf-tufan@users.noreply.github.com>
2025-09-22 11:01:18 +0300

    Time: 3 ms (66.77%), Space: 12.9 MB (16.62%) - LeetHub

--- 2083-three-divisors/2083-three-divisors.py
+++ 2083-three-divisors/2083-three-divisors.py
@@ -4,8 +4,6 @@ class Solution(object):
         for i in range(1,n+1):
             if n%i==0:
                 count+=1
-            else:
-                continue
         if count==3:
             return True
         return False
