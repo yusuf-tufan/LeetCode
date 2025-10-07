12bc80176370df6ed126570fd2451c2064c33e5d
Yusuf Tufan <102178687+yusuf-tufan@users.noreply.github.com>
2025-10-07 18:28:05 +0300

    Time: 257 ms (43.51%), Space: 25.8 MB (25.6%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.py
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -5,5 +5,5 @@ class Solution(object):
         if len(nums)>=2:
             for i in range(0,len(nums)-1):
                 new_list.append(abs(nums[i]-nums[i+1]))
-            return max(new_list)
+            return max(set(new_list))
         return 0
