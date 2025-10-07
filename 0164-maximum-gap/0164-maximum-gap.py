d292c9cafd1bc152babe9587710798c9f510dbc8
Yusuf Tufan <102178687+yusuf-tufan@users.noreply.github.com>
2025-10-07 18:27:28 +0300

    Time: 245 ms (52.88%), Space: 23.9 MB (40.62%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def maximumGap(self, nums):
+        nums.sort()
+        new_list=[]
+        if len(nums)>=2:
+            for i in range(0,len(nums)-1):
+                new_list.append(abs(nums[i]-nums[i+1]))
+            return max(new_list)
+        return 0
