9612a13fb1cb8283ac471b40c45b7b170834d8d6
Yusuf Tufan <102178687+yusuf-tufan@users.noreply.github.com>
2025-10-10 22:35:24 +0300

    Time: 18 ms (5.3%), Space: 12.3 MB (82.14%) - LeetHub

---
+++ 1137-height-checker/1137-height-checker.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def heightChecker(self, heights):
+        new_list=[i for i in heights]
+        count=0
+        heights.sort()
+        print(new_list,heights)
+        for x in range(0,len(new_list)):
+            if heights[x] !=new_list[x]:
+                count+=1
+        return count
