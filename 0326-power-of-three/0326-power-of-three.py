5935ccc8a9bf2ebcadec0da7e9ee8bab00d955b1
Yusuf Tufan <102178687+yusuf-tufan@users.noreply.github.com>
2025-10-11 17:51:01 +0300

    Time: 223 ms (5.21%), Space: 12.5 MB (52.61%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        c=3
+        k=-3
+        if n==-3:
+            return False
+        for i in range(50):
+            c*=3
+            if i%2!=0:
+                k**i
+            if c==n or k==n or n==1 or n==3:
+                return True
+                break
+        return False
