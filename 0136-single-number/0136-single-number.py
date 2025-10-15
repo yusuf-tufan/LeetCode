cbf7d57c04219d7b6385b593c297b2c67d641444
Yusuf Tufan <102178687+yusuf-tufan@users.noreply.github.com>
2025-10-15 21:55:13 +0300

    Time: 6 ms (47.28%), Space: 14.7 MB (17.92%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        new_nums=set()
+        nums_num=set()
+        for i in nums:
+            if i in new_nums:
+                nums_num.add(i)
+            new_nums.add(i)
+        for c in nums_num:
+            new_nums.remove(c)
+        new_nums=list(new_nums)
+        return new_nums[0]
+        
