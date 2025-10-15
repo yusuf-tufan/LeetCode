6166d0777013f6ed5552a8dce6cb329293bb53cc
Yusuf Tufan <102178687+yusuf-tufan@users.noreply.github.com>
2025-10-15 18:27:55 +0300

    Time: 2 ms (43.23%), Space: 12.3 MB (86.59%) - LeetHub

--- 3194-find-words-containing-character/3194-find-words-containing-character.py
+++ 3194-find-words-containing-character/3194-find-words-containing-character.py
@@ -4,5 +4,4 @@ class Solution(object):
         for i in range(0,len(words)):
             if x in words[i]:
                 new_list.append(i)
-        new_list=list(set(new_list))
-        return (new_list)
+        return new_list
