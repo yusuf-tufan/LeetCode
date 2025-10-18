05267c74de435e6b4f6606ce115d54291b05029b
Yusuf Tufan <102178687+yusuf-tufan@users.noreply.github.com>
2025-10-18 14:02:24 +0300

    Time: 0 ms (100%), Space: 17.9 MB (83.33%) - LeetHub

--- 3931-process-string-with-special-operations-i/3931-process-string-with-special-operations-i.py
+++ 3931-process-string-with-special-operations-i/3931-process-string-with-special-operations-i.py
@@ -2,7 +2,6 @@ class Solution(object):
     def processStr(self, s):
         new_s=""
         for i in s:
-            print(i)
             if i.isalpha():
                 new_s+=i
             elif i=='*':
