4ed8ccfbc165bf59c10d88d7b91fb2d36fb3b088
Yusuf Tufan <102178687+yusuf-tufan@users.noreply.github.com>
2026-02-21 14:24:10 +0300

    Time: 11 ms (54.9%), Space: 12.6 MB (22.55%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def reverseDegree(self, s):
+        letters ={'a': 26, 'b': 25, 'c': 24, 'd': 23, 'e': 22, 'f': 21, 'g': 20, 'h': 19, 'i': 18, 'j': 17, 'k': 16, 'l': 15, 'm': 14, 'n': 13, 'o': 12, 'p': 11, 'q': 10, 'r': 9, 's': 8, 't': 7, 'u': 6, 'v': 5, 'w': 4, 'x': 3, 'y': 2, 'z': 1}
+        result=0
+        for i in range(0,len(s)):
+            result+=letters[s[i]]*(i+1)
+        return result
