5916e93b99777634836b067589149b16305d949e
Yusuf Tufan <102178687+yusuf-tufan@users.noreply.github.com>
2026-03-20 00:00:01 +0300

    Time: 0 ms (100%), Space: 7.8 MB (47.92%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public:
+        bool isPowerOfFour(int n) {
+            while (n % 4 == 0 && n > 0) {
+                n/=4;
+            };
+            if (n == 1) {
+                return true;
+            }
+            return false;
+        }
+    };
